1c464cc81803b01cb23cfb4ca3fab81cd35e5372
Hunji <Hungnmnm@gmail.com>
2020-05-27 20:32:03 +0700

    fix error split null 2

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,8 @@ const htmlToText = require('html-to-text');
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
-    this.firstName = user.name.split(' ')[user.name.split(' ') - 1];
+    //this.firstName = user.name.split(' ')[user.name.split(' ') - 1];
+    this.firstName = user.name.split(' ')[0];
     this.url = url;
     this.from = `HunJi <${process.env.EMAIL_FROM}>`;
   }
